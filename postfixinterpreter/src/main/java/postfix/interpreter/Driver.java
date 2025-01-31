1f41f3288f0c2380e9aa5cf272e577192b6c2701
Juan Salguero <sal24374@uvg.edu.gt>
2025-01-30 20:44:00 -0600

    Documentación

--- postfixinterpreter/src/main/java/postfix/interpreter/Driver.java
+++ postfixinterpreter/src/main/java/postfix/interpreter/Driver.java
@@ -6,18 +6,19 @@ public class Driver {
     public static void main(String[] args) {
         Calculadora calcu = new Calculadora();
         ArrayList<String[]> tokens = new ArrayList<>();
+        // Leer el archivo de entrada
         try{
             tokens = calcu.readFile("datos.txt");
         }catch(IOException e){
             System.out.println("Error al leer el archivo: " + e.getMessage());
             return;
         }
-
+        // Verificar si el archivo estaba vacío
         if (tokens.size() == 0) {
             System.out.println("No hay ninguna operación en el archivo.");
             return;
         }
-
+        // Evaluar la expresión 
         int[] resultados = new int[tokens.size()];
         for (int i = 0; i < tokens.size(); i++) {
             resultados[i] = calcu.interpret(tokens.get(i));

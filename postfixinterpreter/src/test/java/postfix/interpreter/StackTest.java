4eca89b5197e8f41df8eb12f9238edc558f43dcd
Samuuu132 <gud24451@uvg.edu.gt>
2025-01-30 21:02:17 -0600

    Es el test para comprobar de que occure cuando la exprension esta escrita incorrectamente

--- postfixinterpreter/src/test/java/postfix/interpreter/StackTest.java
+++ postfixinterpreter/src/test/java/postfix/interpreter/StackTest.java
@@ -1,16 +1,17 @@
 package postfix.interpreter;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
-/**
- * 
- */
-public class StackTest {
-    // TODO: Implementar tests necesarios para el Stack.
+
+class CalculadoraTest {
+
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    void testQuedanNumerosEnLaPila() {
+        Calculadora calculadora = new Calculadora();
+        String[] input = {"5", "3", "+" , "2"}; // Expresión mal formada
+        Exception exception = assertThrows(IllegalStateException.class, () -> {
+            calculadora.interpret(input);
+        });
+        assertEquals("Error: Expresión mal formada, quedan elementos en la pila.", exception.getMessage());
     }
-}
